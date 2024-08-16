2bc7a41a56ebbb86f2247179b6de6ea531a519eb
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-16 18:14:41 +0530

    BACK

--- express/index.js
+++ express/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const app= express();
-console.dir(app);
 
-app.use((req,res) => {
+
+app.use((req,res,next) => {
     console.log(" WE GOT A NEW REQUEST! ");
+    next();
+});
     // res.send("HELLO, WE GOT YOUR REQUEST! THIS FEELS AMAZING!! ");
     // res.send({color:'red'});
-    res.send('<h1>This is my webpage!</h1>');
-    
-})
+    // res.send('<h1>This is my webpage!</h1>');
+   
+
+    app.get('/', (req,res)=>{
+        res.send("okay");
+    });
+
+
+    app.get('/cats',(req,res)=>{
+        res.send("MEOW!!!");
+    });
+
+    app.get('/dogs',(req,res) =>{
+        res.send("WOOF!!!");
+    });
+
+
+    //cats => 'meow'
+    //dogs => 'woof'
+    // 
+
 
 app.listen(8080, () =>{
     console.log("LISTENING ON PORT 8080!");
-})
+});
