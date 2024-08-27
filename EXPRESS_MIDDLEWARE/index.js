8719ddae80877cd4640a7a6ee03a3ede6b76446b
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-27 13:59:01 +0530

    middleware

--- EXPRESS_MIDDLEWARE/index.js
+++ EXPRESS_MIDDLEWARE/index.js
@@ -15,3 +15,19 @@ app.get("/", function(req, res) {
 });
 
 app.listen(8080, () => console.log("server running at port 8080"));
+
+
+// app.get('/user/:id', (req, res, next) => {
+//   // if the user ID is 0, skip to the next route
+//   if (req.params.id === '0') next('route')
+//   // otherwise pass the control to the next middleware function in this stack
+//   else next()
+// }, (req, res, next) => {
+//   // send a regular response
+//   res.send('regular')
+// })
+
+// // handler for the /user/:id path, which sends a special response
+// app.get('/user/:id', (req, res, next) => {
+//   res.send('special')
+// })
